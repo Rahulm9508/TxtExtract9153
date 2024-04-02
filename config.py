d12f9d32698c94b01170d1ed558c145750712be5
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-04-02 17:43:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6468156597:AAHoRLyL6dVWSUxYLlYetwBxCuxZmDxoQ0Q"
+    API_ID = "20319884"
+    API_HASH = "637e3ba6357aa3ba2f3bf5742e0fd066"
+    AUTH_USERS = '1183124209'
+    
