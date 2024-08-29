9e9a9e2516ce12e8ef3c3fb012b197bcd757bb3e
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-08-29 22:10:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "7012630167:AAFOavCBDBP_xUvvi8Z4nHfnHbE7mLc8aBg"
+    BOT_TOKEN = "7358697038:AAHZzDPxqQkK4jAVA-yOErN3yFh10vlzuhE"
     API_ID = "20346550"
     API_HASH = "bc79c3bea7a626887bdc0871eecf0327"
-    AUTH_USERS = '7491167754'
+    AUTH_USERS = '7081036509'
     
